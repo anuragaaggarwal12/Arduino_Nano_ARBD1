ad668afa45a72624df712c528cdbeff592463443
anuragaaggarwal12 <52050457+anuragaaggarwal12@users.noreply.github.com>
2019-06-21 23:36:12 +0530

    new

--- Monitoring_System/logging.py
+++ Monitoring_System/logging.py
@@ -5,21 +5,23 @@ import serial
 import thingspeak
 from time import sleep
 
-mych=thingspeak.Channel(806491,api_key="Z7I1KDZEYOMNS6I8")
+mych=thingspeak.Channel(806491,api_key="Z7I1KDZEYOMNS6I8")     #(channel_id, api_key)
 
-arduino=serial.Serial('COM3',9600)
+arduino=serial.Serial('COM3',9600)       #data is sent from controller to python
 
 arduino.flushInput()
 for i in range(10):
-    sleep(1)
+    sleep(1)                             #give delay of 1 mili sec
     a=int(arduino.readline()[:-2])
     b=int(arduino.readline()[:-2])
     c=int(arduino.readline()[:-2])
-    d=int(arduino.readline()[:-2])
-    data = {'field1':a,'field2':b,'field3':c,'field4':d}
+    
+    data = {'field1':a,'field2':b,'field3':c}      #jason format
     print(data)
-    up = mych.update(data)      #jason format
-    while(up == '0'):
-        up = mych.update({'field1':a,'field2':b,'field3':c,'field4':d})
+    up = mych.update(data)                
 
-arduino.close()     #to close com3 port of arduino
+    #while channel is busy then wait and when channel is not busy update the value         
+    while(up == '0'):
+        up = mych.update(data)
+
+arduino.close()                           #to close com3 port of arduino
